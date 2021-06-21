2d3ce5e3d0a2293630810154570246f19c732824
TerryL1971 <tcl71771@yahoo.com>
2021-06-21 15:49:25 +0200

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -24,7 +24,7 @@ export class MainView extends React.Component {
     });
   }
 
-  componentDidMount(){
+/*  componentDidMount(){
     axios.get('https://myflix-app-2021.herokuapp.com/movies')
       .then(response => {
         this.setState({
@@ -34,7 +34,22 @@ export class MainView extends React.Component {
       .catch(error => {
         console.log(error);
       });
-    }
+    } */
+
+  getMovies(token) {
+    axios.get('https://myflix-app-2021.herokuapp.com/movies', {
+      headers: { Authorization: `Bearer ${token}`}
+    })
+    .then(response => {
+      // Assign the result to the state
+      this.setState({
+        movies: response.data
+      });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
  
 
   /*When a movie is clicked, this function is invoked and updates the state of the `selectedMovie` *property to that movie*/
@@ -47,10 +62,15 @@ export class MainView extends React.Component {
 
 /* When a user successfully logs in, this function updates the `user` property in state to that *particular user*/
 
-  onLoggedIn(user) {
-    this.setState({
-      user
+  onLoggedIn(authData) {
+  console.log(authData);
+  this.setState({
+    user: authData.user.Username
     });
+
+  localStorage.setItem('token', authData.token);
+  localStorage.setItem('user', authData.user.Username);
+  this.getMovies(authData.token);
   }
 
 
