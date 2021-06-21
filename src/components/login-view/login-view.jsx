fa3c3307e13e3c25e58f68d142e92fb7a7b17784
TerryL1971 <tcl71771@yahoo.com>
2021-06-21 08:50:06 +0200

    Updated main-view.jsx for Achievement 3.4

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -4,9 +4,10 @@ export function LoginView(props) {
   const [ username, setUsername ] = useState('');
   const [ password, setPassword ] = useState('');
 
-  const onLoggedIn = props
+  const { onLoggedIn } = props
 
-  const handleSubmit = () => {
+
+  const handleSubmit = (e) => {
     e.preventDefault();
     console.log(username, password);
     /* Send a request to the server for authentication */
